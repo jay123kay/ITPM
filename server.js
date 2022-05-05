2283e69ff77176ea299f79651349f47ae225f5d8
IT20216764 <88416028+jay123kay@users.noreply.github.com>
2022-05-05 23:12:40 +0530

    Jayasekara K.D

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//import routes
+const bookingRoutes = require('./routes/bookings');
+const paymentRoutes = require('./routes/payments');
+
+
+//app middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(bookingRoutes);
+app.use(paymentRoutes);
+
+
+const PORT = 8000;
+const DB_URL ='mongodb+srv://Kavya:itpm@itpm.8d8uh.mongodb.net/ITPM?retryWrites=true&w=majority'
+
+mongoose.connect(DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+
+})
+.then(() =>{
+    console.log('DB connected');
+})
+.catch((err) => console.log('DB connection error', err))
+
+app.listen(PORT, ()=>{
+    console.log(`App is running on ${PORT}`);
+});
