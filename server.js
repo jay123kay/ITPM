e443b883ab403073c96f69d7814e5b745f0f7ccc
Samarawickrama G.U.N <89275902+GaveshaniSam@users.noreply.github.com>
2022-05-08 16:14:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,27 @@ const app = express();
 //import routes
 const postRoutes = require('./routes/posts');
 const orderRoutes = require('./routes/orders');
+const stockRoutes = require('./routes/stocks');
+const fruitRoutes = require('./routes/fruits');
+const breadRoutes = require('./routes/breads');
+const employeeRoutes = require('./routes/employees');
+const serverRoutes = require('./routes/servers');
+const loginRoutes = require('./routes/logins');
 
 //app middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(postRoutes);
-app.use(orderRoutes);
 
 //route middleware
 app.use(postRoutes);
 app.use(orderRoutes);
+app.use(stockRoutes);
+app.use(fruitRoutes);
+app.use(employeeRoutes);
+app.use(serverRoutes);
+app.use(breadRoutes);
+app.use(loginRoutes);
 
 const PORT = 8000;
 const DB_URL = 'mongodb+srv://Sarangi:itpm@itpm.8d8uh.mongodb.net/ITPM?retryWrites=true&w=majority';
