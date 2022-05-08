789dc371c449a5c42923eec12f62b5d2cf1d9a84
Samarawickrama G.U.N <89275902+GaveshaniSam@users.noreply.github.com>
2022-05-08 15:27:40 +0530

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -13,7 +13,7 @@ export default class NavBar extends Component {
            <ul >
             <li></li>  
             <li></li> 
-           <li><a href="/login"><i className="fas fa-home"></i>Home</a></li>
+           <li><a href="/home"><i className="fas fa-home"></i>Home</a></li>
                 <li><a href="#"><i class="fas fa-gifts"></i>Food and beverage</a></li> 
                 <li><a href="/mainb"><i class="fas fa-gifts"></i>Banquet Hall</a></li>
                 <li><a href="#"><i class="fas fa-info"></i>About</a></li>
