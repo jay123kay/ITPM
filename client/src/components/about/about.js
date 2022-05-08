1e271b5c73153b6fc47c01adbfaddbb5537e7fe2
Samarawickrama G.U.N <89275902+GaveshaniSam@users.noreply.github.com>
2022-05-08 18:49:27 +0530

    Add files via upload

---
+++ client/src/components/about/about.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import "./about.css"
+ 
+function about() {
+    return(
+        <div className='bodyb'>
+
+
+        <center>
+            <h2>
+                <u>About</u>
+            </h2>
+        </center>
+
+      
+<div className="aboutStyle">
+
+            <center>
+                <h5><u>About</u></h5>
+                <br></br>
+          </center>
+          <hr></hr>
+
+            <p>
+                <h6>
+
+                    FOOD O'CLOCK is a restaurant designed around the haul, featuring a menu
+                    that changes daily with a focus on seasonal and local cuisine. Heirloom
+                    grains are milled fresh for house made pasta, breads, and desserts, and an 
+                    extensive in house fermentation program capture flavors at their peak and 
+                    preserves them for the off season. 
+                    
+                   
+                </h6>
+            </p>
+
+            <p>
+                <h6>
+                    <br></br>
+                    
+                    When you visit the restaurant check out our Foods. There are two ways to dine with us. One off the menu wich offers small
+                    plates, pastas and dessert wich are all meant to be shared. In addition to ourprinted menu, guests can also order off circulating
+                     cart that stop by during meal time 
+                </h6>
+            </p>
+<h5>Uses...</h5>
+            <p>
+                <h6>
+                    <br></br>
+                    It reduces the time and manpower required for management and maintenance of 
+                    different tasks. It reduces the paper work in existing system, hence it is economical and 
+                    efficient. Also it will help to keep health guidelines as far as possible. Using this system 
+                    customer can manage their day today works done with restaurant easily and without a 
+                    risk. Not only customers but also managers can maintain the restaurant easily and also
+                    using this system, restaurant can increase the attractiveness of their services.
+                </h6>
+            </p>
+<br></br><br></br>
+        <center>
+            
+        </center>   
+  </div>
+
+  </div>
+    )
+    
+}
+
+export default about
